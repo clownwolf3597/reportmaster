ac639ede4df3ec1015c3eefd308fba61b4f2856e
clownwolf3597 <saravansomanchi3597@gmail.com>
2022-10-07 01:10:51 +0530

    header condition changes

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,11 +10,6 @@ export class AppService {
     isLoginPage: boolean = false;
 
     constructor(private router: Router) {
-        this.router.events.subscribe((value: any) => {
-            if (value instanceof NavigationEnd) {
-                this.isLoginPage = (value?.url === '/login');
-            }
-        });
-
+        this.isLoginPage = !window.location.href.includes('app');
     }
 }
